7a6b73e0b61e311277fe2467e90323785b2c0c11
Sami Wannell <swannell@redcross.org.uk>
2020-05-15 16:32:14 +0100

    Cleaned up file to make it 100% mean-based

--- ttest_MDE.py
+++ ttest_MDE.py
@@ -8,34 +8,26 @@ Created on Fri May 15 13:45:34 2020
 import numpy as np
 from scipy.stats import ttest_ind_from_stats
 
-params = {'ctrl': {'prompts': [5, 25, 50],
-                   'p': [0.35, 0.35, 0.3]},
-#          'test': {'prompts': [15, 35, 50],
-#                   'p': [0.35, 0.35, 0.3]}
-          }
+params = {'prompts': [5, 25, 50],
+          'p': [0.35, 0.35, 0.3],
+          'vol': 1000}
 
-params['ctrl']['vol'] = 1000
-ctr_drop = 0.9
-#params['test']['vol'] = int(params['ctrl']['vol'] * ctr_drop)
-
-for cell in params.keys():
-    params[cell]['vals'] = np.random.choice(params['ctrl']['prompts'],
-                                            size=params['ctrl']['vol'],
-                                            p=params['ctrl']['p'])
-    params[cell]['mean'] = params[cell]['vals'].mean()
-    params[cell]['std'] = params[cell]['vals'].std()
+params['vals'] = np.random.choice(params['prompts'], size=params['vol'],
+                                  p=params['p'])
+params['mean'] = params['vals'].mean()
+params['std'] = params['vals'].std()
 
 p_MDE = 1
 t_MDE = 0
 expected_liftMDE = 1.01
 while p_MDE > 0.05:
     t_MDE, p_MDE = ttest_ind_from_stats(
-            params['ctrl']['mean'],
-            params['ctrl']['std'],
-            params['ctrl']['vol'],
-            params['ctrl']['mean']*expected_liftMDE,
-            params['ctrl']['std'],
-            params['ctrl']['vol']
+            params['mean'],
+            params['std'],
+            params['vol'],
+            params['mean']*expected_liftMDE,
+            params['std'],
+            params['vol']
             )
     if p_MDE < 0.05:
         break
@@ -43,15 +35,15 @@ while p_MDE > 0.05:
 
 print(t_MDE, p_MDE, expected_liftMDE)
 
-degf = sum([params[cell]['vol'] for cell in params.keys()]) - 2
+degf = params['vol']*2 - 2
 upliftMDE = (expected_liftMDE-1)*100
 
 print("Ctrl: (mean={:.2f}, std={:.2f})".format(
-              params['ctrl']['mean'],
-              params['ctrl']['std']),
+              params['mean'],
+              params['std']),
       "\nTest: (mean={:.2f}, std={:.2f})".format(
-              params['ctrl']['mean']*expected_liftMDE,
-              params['ctrl']['std']),
+              params['mean']*expected_liftMDE,
+              params['std']),
       "\nt({:.0f})={:.2f}, p={:.2f}, uplift={:,.0f}%".format(
               degf,
               t_MDE,
